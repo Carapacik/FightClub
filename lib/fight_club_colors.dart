b5d8993448f0f8b525e02519feada104a9b48439
Roman <carapacik@gmail.com>
2021-06-12 13:48:54 +0300

    another fix

--- lib/fight_club_colors.dart
+++ lib/fight_club_colors.dart
@@ -8,7 +8,7 @@ class FightClubColors {
   static const Color greyButton = Colors.black38;
   static const Color blueButton = Color.fromRGBO(28, 121, 206, 1);
   static const Color blackButton = Colors.black87;
-  static const Color darkGreyText = Color.fromRGBO(6, 13, 20, 1);
+  static const Color darkGreyText = Color.fromRGBO(22, 22, 22, 1);
   static const Color whiteText = Color.fromRGBO(255, 255, 255, 0.87);
   static const Color descriptionBackground = Color.fromRGBO(198, 208, 229, 1);
 }
