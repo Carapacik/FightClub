9c4aa0b2d4fca31891502fa8a495337ba8604f94
Roman <carapacik@gmail.com>
2021-07-04 13:19:42 +0300

    Release

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,11 @@
 import 'package:fightclub/pages/main_page.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:google_fonts/google_fonts.dart';
 
-void main() {
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await SystemChrome.setPreferredOrientations([DeviceOrientation.portraitUp]);
   runApp(MyApp());
 }
 
